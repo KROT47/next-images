cfba07b3c1f152eb24b5888ed33d805d96ff542c
Dmitry Leleko <dleleko@tango.me>
2020-08-21 20:43:16 +0300

    Update to 2.0

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-module.exports = (nextConfig = {}) => {
+const DefaultConfig = {
+  test: /\.(jpe?g|png|svg|gif|ico|webp|jp2)$/,
+  // Next.js already handles url() in css/sass/scss files
+  issuer: /\.\w+(?<!(s?c|sa)ss)$/i,
+  name: "[name]-[hash].[ext]",
+};
+
+module.exports = (getImagesConfig = (x) => x) => (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
       const { isServer } = options;
-      nextConfig = Object.assign({ inlineImageLimit: 8192, assetPrefix: "" }, nextConfig);
+      nextConfig = Object.assign(
+        { inlineImageLimit: 8192, assetPrefix: "" },
+        nextConfig
+      );
 
       if (!options.defaultLoaders) {
         throw new Error(
-          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        )
+          "This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+        );
       }
 
+      const { test, issuer, name } = getImagesConfig(DefaultConfig);
+
       config.module.rules.push({
-        test: /\.(jpe?g|png|svg|gif|ico|webp|jp2)$/,
-        // Next.js already handles url() in css/sass/scss files
-        issuer: /\.\w+(?<!(s?c|sa)ss)$/i,
+        test,
+        issuer,
         exclude: nextConfig.exclude,
         use: [
           {
@@ -23,18 +34,18 @@ module.exports = (nextConfig = {}) => {
               fallback: require.resolve("file-loader"),
               publicPath: `${nextConfig.assetPrefix}/_next/static/images/`,
               outputPath: `${isServer ? "../" : ""}static/images/`,
-              name: "[name]-[hash].[ext]",
-              esModule: nextConfig.esModule || false
-            }
-          }
-        ]
+              name,
+              esModule: nextConfig.esModule || false,
+            },
+          },
+        ],
       });
 
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
       }
 
-      return config
-    }
-  })
-}
+      return config;
+    },
+  });
+};
